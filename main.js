5f793c001eb6ae2b8894bdfc00b7e0243eb004c7
Your Name <mackenziewbryan@gmail.com>
2019-05-14 21:01:21 -0500

    Adjust states links object

--- main.js
+++ main.js
@@ -3,16 +3,47 @@ import Content from './Components/Content';
 import Footer from './Components/Footer';
 
 import img from './DSC_7723.jpg';
+import { link } from 'fs';
 
-const state = {
-    'image': img
+const states = {
+    'home': {
+        'title': 'Succotash',
+        'image': img,
+        'links': {'primary': ['Home', 'About', 'Contact'],
+      'dropdown': ['Class Project', 'Hack-a-Thon', 'Demo']}
+    },
+    'contact': {
+        'title': 'Contact me!',
+        'image': img,
+        'links': {'primary': ['Home', 'About', 'Contact'],
+        'dropdown': ['Class Project', 'Hack-a-Thon', 'Demo']}
+    },
+    'about': {
+        'title': 'About me',
+        'image': img,
+        'links': {'primary': ['Home', 'About', 'Contact'],
+        'dropdown': ['Class Project', 'Hack-a-Thon', 'Demo']}
+    }
 };
 
-state.title = 'Succotash';
+const root = document.querySelector('#root');
 
-document.querySelector('#root').innerHTML = `
+function render(state){
+    root.innerHTML = `
 ${Navigation(state)}
 ${Content(state)}
 ${Footer(state)}
 `;
+}
 
+
+render(states.home);
+
+const links = document.querySelectorAll('nav a');
+
+links.forEach((link) => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        render(states[`${e.target.textContent.toLowerCase()}`]);
+    });
+});
