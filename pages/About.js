34d8dab0ce450617212a1eb6a2f754ce16bd6402
Your Name <mackenziewbryan@gmail.com>
2019-05-20 19:49:07 -0500

    Add about content from old page

--- pages/About.js
+++ pages/About.js
@@ -1,6 +1,20 @@
-  
-  
-export default function(){
-    return `<h1>About Me!</h1>
-  <p>This is a paragraph</p>`;
+export default function() {
+  return `<main class="container">
+    <section>
+      <p>
+        Mackenzie Bryan is a mycologist, Permaculture designer, bike mechanic, and chef seeking to expand his horizons into a new field. With his experiences in project management and problem solving, coding seemed the most logical step to continue the trajectory his path is taking. Joining the Savvy Coders Cohort for front-end web development matched the needs and interests to fulfill this dream.
+      </p>
+      <p>
+        Born in London, raised in Chicago, and having visited over 30 countries, Mackenzie has gained enough knowledge to see where his skills can be used. Learning the needs of the world to provide valuable assets has been the intention since leaving school. After having chosen to bypass the traditional schooling route post high-school, Mackenzie knew that education did not stop because school did.
+      </p>
+      <p>
+        Mackenzie viewed the world as a classroom and this idea led to many an adventure along with countless learning experiences that are constantly ongoing. Focusing mostly on practical knowledge in earth-based systems and mechanical knowledge took him to the eastern border of Nepal to study traditional tea farming, also to rural India to join the international community of thinkers opting to search for solutions at the International Permaculture Convergence. These are two examples of a list that is forever growing, if you want to learn more visit the <a href="contact.html">contact page</a>!
+      </p>
+      <p>
+        These experiences have shown him the importance of web-development and other forms of computer programming in the world. Being trained in this field opens up a whole new realm of possibilities which he would love to be a part. He would love to hear from you about his projects and possible collaborations in the future!
+      </p>
+    </section>
+
+    </section>
+  </main>`;
 }
